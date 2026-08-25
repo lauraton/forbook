a21eac15aed599544e964e70d57e7d58c6fc3a1d
lauraton <nickk5926@gmail.com>
2026-08-25 16:40:39 -0300

    WIP: se inició con el desarrollo de la tabla usuario, pero todavía faltan las validaciones y controladores.

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,2 @@
+import { sequelize } from "../config/database";
+import { User } from "../models/user.model";
---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+import { sequelize } from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+export const User = sequelize.define("User", {
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    surname: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    email: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        unique: true
+    },
+    
+    password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+
+    },
+
+    phone: {
+        type: DataTypes.INTEGER(14),
+        allowNull: true,
+        unique: true
+
+    }
+})
